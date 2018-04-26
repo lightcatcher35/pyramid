1d8fd820952711535391ea78562d3aa00b291176
ilker özer <ilkerozer35@gmail.com>
2018-04-26 11:13:08 +0300

    Wifi_temel

---
+++ wifi/wifi.ino
@@ -0,0 +1,12 @@
+#define ag_ismi "ilkeriphone"
+#define ag_sifresi "12345679"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}<
