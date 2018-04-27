10e80056d67e5afac4d8c5d457794b75d8fbecf3
ilker özer <ilkerozer35@gmail.com>
2018-04-27 09:13:18 +0300

    Wifi kontrol programı

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,12 +1,32 @@
 #define ag_ismi "ilkeriphone"
 #define ag_sifresi "12345679"
+#define IP "184.106.153.149"    //thingspeak.com IP adresi
 
-void setup() {
-  // put your setup code here, to run once:
+float sicaklik;
 
+void setup()
+{
+  Serial.begin(115200); //Seriport'u açıyoruz. Güncellediğimiz
+  //ESP modülünün baudRate değeri 115200 olduğu için bizde Seriport'u 115200 şeklinde seçiyoruz
+
+  Serial.println("AT"); //ESP modülümüz ile bağlantı kurulup kurulmadığını kontrol ediyoruz.
+
+  delay(3000); //ESP ile iletişim için 3 saniye bekliyoruz.
+  analogReference(INTERNAL);
+  if (Serial.find("OK")) {
+
+    
+    Serial.println("esp ile bağlanıldı");
+
+
+  } else
+  {
+    Serial.println("esp ile bağlantı kurulamadı");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
 
-}<
+
+
